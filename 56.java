5fc47cba346257396a2f908d9de8ff74bdd4e82b
Valdir <45928596+vdsj@users.noreply.github.com>
2021-08-31 21:23:28 -0300

    Update 56.java

--- 56.java
+++ 56.java
@@ -1,24 +1,15 @@
-public int findMinCutCost(int[] m, int n) {
-   int cost = n * m.length;
-   for (int i=0; i<m.length; i++) {
-      cost = Math.min(findMinCutCostImpl(m, n, i), cost);
-   }
-   return cost;
-}
-
-private int findMinCutCostImpl(int[] m, int n, int i) {
+private int min_cut_cost_imp(int i, int[] m, int n) {
+   int cl = 0, cr = 0;
    if (m.length == 1) 
      return n;
-   int cl = 0, cr = 0;
    if (i > 0) {
       cl = Integer.MAX_VALUE;
       int[] ml = Arrays.copyOfRange(m, 0, i);
       int nl = m[i];
       for (int j=0; j<ml.length; j++) {
-         cl = Math.min(findMinCutCostImpl(ml, nl, j), cl);
+         cl = Math.min(min_cut_cost_imp(ml, nl, j), cl);
       }
-   }
-   if (i < m.length - 1) {
+   } else if (i < m.length - 1) {
       cr = Integer.MAX_VALUE;
       int[] mr = Arrays.copyOfRange(m, i + 1, m.length);
       int nr = n - m[i];
@@ -26,8 +17,16 @@ private int findMinCutCostImpl(int[] m, int n, int i) {
          mr[j] = mr[j] - m[i];
       }
       for (int j=0; j<mr.length; j++) {
-         cr = Math.min(findMinCutCostImpl(mr, nr, j), cr);
+         cr = Math.min(min_cut_cost_imp(mr, nr, j), cr);
       }
    }
    return n + cl + cr;
+}
+
+public int min_cut_cost(int[] m, int n) {
+   int cost = n * m.length;
+   for (int i=0; i<m.length; i++) {
+      cost = Math.min(min_cut_cost_imp(m, n, i), cost);
+   }
+   return cost;
 }
